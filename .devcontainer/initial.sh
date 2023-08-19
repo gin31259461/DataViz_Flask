029c3d039119f1a6f3788f1097ecb7bd3e259b71
abner <qw0207060413@gmail.com>
2023-08-19 06:01:44 +0000

    Change devcontainer setting

--- .devcontainer/initial.sh
+++ .devcontainer/initial.sh
@@ -4,3 +4,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/$UBUNTU_VERSION/prod jammy main" | tee /etc/apt/sources.list.d/mssql-release.list
 apt update
 apt-get install -y msodbcsql17
+
+apt list --upgradable
+apt-get install -y libexpat1-dev
+apt-get install -y libpango1.0-dev
