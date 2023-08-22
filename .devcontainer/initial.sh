eec34563ce59ae5925daf42f0c9c4d9a292c126e
abner <qw0207060413@gmail.com>
2023-08-22 06:57:16 +0000

    Backup work

--- .devcontainer/initial.sh
+++ .devcontainer/initial.sh
@@ -1,7 +1,7 @@
 # Install mssql odbc 17 driver
-UBUNTU_VERSION=22.04
+DEBIAN_VERSION=11
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/$UBUNTU_VERSION/prod jammy main" | tee /etc/apt/sources.list.d/mssql-release.list
+echo "deb [arch=amd64] https://packages.microsoft.com/debian/$DEBIAN_VERSION/prod bullseye main" | tee /etc/apt/sources.list.d/mssql-release.list
 apt update
 apt-get install -y msodbcsql17
 
