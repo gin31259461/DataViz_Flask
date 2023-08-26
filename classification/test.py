026b965d8e20b39a385d53e802a35c6e636a69a5
abner <qw0207060413@gmail.com>
2023-08-26 07:39:25 +0000

    Backup work

--- classification/test.py
+++ classification/test.py
@@ -2,14 +2,17 @@ import sys
 from pathlib import Path
 
 print(Path.cwd())
-sys.path.append(f"{Path(Path.cwd()).parent.absolute()}/")
+sys.path.append(f"{Path.cwd().absolute()}/")
 import pandas as pd
-from sqlalchemy import text
 
 from setup import setup
 
 db = setup()
-result = db.execute(text("select * from RawDB.dbo.D139"))
-df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+df = pd.DataFrame(pd.date_range("2000-01-02", freq="1D", periods=15), columns=["Date"])
+
+bins_dt = pd.date_range("2000-01-01", freq="3D", periods=6)
+bins_str = bins_dt.astype(str).values
 
 print(df)
+print(bins_dt)
